e090f29a0aa6eb19545d4a370e4fb261b86bae3d
Kogel-code <60623050+Kogel-code@users.noreply.github.com>
2020-02-10 14:28:29 -0500

    Started Code

---
+++ HAJA.py
@@ -0,0 +1,11 @@
+team_name = 'HAJA'
+strategy_name = ''
+strategy_description = ''
+
+import random
+
+def move(my_history, their_history, my_score, their_score):
+    if 'b' in their_history
+        return 'b'
+    else:   
+        return 'c' 
