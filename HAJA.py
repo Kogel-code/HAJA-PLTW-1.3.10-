10aef3ac42b3b5d485082954e724fab70f4c1972
Kogel-code <60623050+Kogel-code@users.noreply.github.com>
2020-02-12 13:27:07 -0500

    Worked on code

--- HAJA.py
+++ HAJA.py
@@ -1,11 +1,10 @@
 team_name = 'HAJA'
-strategy_name = ''
-strategy_description = ''
+strategy_name = 'Betray When Betrayed'
+strategy_description = 'Will bretray if last move of opponent was betray'
 
-import random
 
 def move(my_history, their_history, my_score, their_score):
-    if 'b' in their_history
+    if 'b' in their_history[-1:]:
         return 'b'
     else:   
         return 'c' 
